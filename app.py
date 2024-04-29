3bf862f9b199a6a9837445c03d4584ef3bb705dc
akhmedovoybek <akhmedovoybek2555@gmail.com>
2024-04-29 22:42:58 +0500

    OpenCV App

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
 import cv2
 from utils import Finger_counter
+from AppOpener import open
+import speech_recognition as sr
 
 def demostration():
-    """It is the demostration of the finger counter project. 
-    It aims to detect your hand then finding out the upper finger count.
-    It calculates the finger count through processing hand ROI which parsed to areas with certain coordinates.
-    """
-
+    r = sr.Recognizer()
     # Creating instances
     finger_counter = Finger_counter()
     cap = cv2.VideoCapture(0)
@@ -24,6 +22,7 @@ def demostration():
         # Find out the finger count which is upen the hand
         drawn_image, finger_count = finger_counter.count_fingers(frame)
 
+
         # displaying the results
         cv2.imshow("Finger Counter", drawn_image) # showing Video 
 
@@ -38,7 +37,15 @@ def demostration():
         if k == ord("s"):
             cv2.imwrite(f"hand_counted_{name_index}.jpg", drawn_image)
             name_index += 1
-
+        # with sr.Microphone() as source:
+        #     audio_data = r.listen(source)
+        #     try:
+        #         text = r.recognize_google(audio_data)
+        #         print("Speech: "+text)
+        #     except sr.UnknownValueError:
+        #         print("Google Speech Recognition could not understand the audio")
+        #     except sr.RequestError as e:
+        #         print("Could not request results from Google Speech Recognition service; {0}".format(e))
     # re-allocating the sources
     cap.release()
     cv2.destroyAllWindows()
--- utils.py
+++ utils.py
@@ -1,20 +1,14 @@
 import cv2
 import mediapipe as mp
 import numpy as np
+import psutil
+from AppOpener import open, close
+import speech_recognition as sr
 
 
 class Finger_counter():
 
     def __init__(self, finger_coor: list = [(8, 6), (12, 10), (16, 14), (20, 18)],  thum_coor: tuple = (4, 3)) -> None:
-        """It is the constructur of the object Finger_counter.
-
-        Parameters
-        ----------
-        finger_coor : list, optional
-            The coordinates which used to parse the ROI into areas. These areas are the expected locations of the fingers.  , by default [(8, 6), (12, 10), (16, 14), (20, 18)]
-        thum_coor : tuple, optional
-           Expected location of the thumb (finger) in the ROI, by default (4, 3)
-        """
 
         # setting the image processing instances
         self.hand = mp.solutions.hands
@@ -26,32 +20,10 @@ class Finger_counter():
         self.thum_coor = thum_coor
 
 
-    def count_fingers(self, image:np.ndarray, text_center_coor:tuple=(150, 150), font:int=cv2.FONT_HERSHEY_PLAIN,  font_scale:int=12, color:tuple=(0, 0, 255), thickness:int = 12)->tuple:
-        """Calculates the total  upped finger counts in the detected hand ROI.
-
-        Parameters
-        ----------
-        image : np.ndarray
-            Target image which will be used to process.
-        text_center_coor : tuple, optional
-            Colour of the text which denotes the total finger count where in the image , by default (150, 150)
-        font : int, optional
-            Writing font of the finger count text, by default cv2.FONT_HERSHEY_PLAIN
-        font_scale : int, optional
-            Writing size of the finger count text, by default 12
-        color : tuple, optional
-            Writing color of the finger count text, by default (0, 0, 255)
-        thickness : int, optional
-            Writing thickness of the finger count text , by default 12
-
-        Returns
-        -------
-        tuple
-            It returns the drawn image and the total finger count.
-        """
-
-        
+    def count_fingers(self, image:np.ndarray, text_center_coor:tuple=(150, 150), font:int=cv2.FONT_HERSHEY_PLAIN,  font_scale:int=12, color:tuple=(0, 255, 255), thickness:int = 12)->tuple:
         # Finding out the Hands in the Frame tgrouh Image Tracking
+        imaged = cv2.imread("image.jpg")
+        cv2.imshow("Documentation", imaged)
         results = self.hands.process(image)
 
         # finding out the finger count throug the hand landmarks
@@ -70,30 +42,24 @@ class Finger_counter():
         # drawing the reults into tge image
         cv2.putText(image, str(finger_count), text_center_coor, font, font_scale, color, thickness)
 
+        if (finger_count==1):
+            if (not "telegram.exe" in (i.name() for i in psutil.process_iter())):
+                open("telegram")
+        elif (finger_count==2):
+            open("chrome")
+        elif (finger_count==3):
+            if not "code.exe" in (i.name() for i in psutil.process_iter()):
+                open("visual studio code")
+        elif (finger_count==4):
+            if not "brave.exe" in (i.name() for i in psutil.process_iter()):
+                open("brave")
+        elif (finger_count==5):
+            if not "wordpad.exe" in (i.name() for i in psutil.process_iter()):
+                open("wordpad")
         return image, finger_count
 
     def _draw_landmarks(self, image:np.ndarray, results:type, landmarks:list, radius:int = 5, color:tuple = (0, 255, 0))->np.ndarray:
-        """It draws the hand landmarks on the image.
 
-        Parameters
-        ----------
-        image : np.ndarray
-            Target image which will be used to draw the landmarks.
-        results : type
-            Source of the landmarks.
-        landmarks : list
-            List of lands mark coordiante
-        radius : int, optional
-            Radius of the circle which will be used to draw landmark points, by default 5
-        color : tuple, optional
-            Color of the landmark circles, by default (0, 255, 0)
-
-        Returns
-        -------
-        np.ndarray
-            Drawn image.
-        """
-        
         # drawing Hand Connections
         for hand_in_frame in results.multi_hand_landmarks:
             self.hand_draw.draw_landmarks(image, hand_in_frame, self.hand.HAND_CONNECTIONS)
@@ -101,25 +67,9 @@ class Finger_counter():
         # drawing the hand land mark points as circles
         for point in landmarks:
             cv2.circle(image, point, radius, color, cv2.FILLED)
-        
+
         return image
-    
     def _check_finger_positions(self, landmarks:list, finger_count:int)->int:
-        """Checks the already parsed coordinates whether a finger is exist or not. According to results it increases the finger count or not.
-
-        Parameters
-        ----------
-        landmarks : list
-            List of the landmark coordinates.
-        finger_count : int
-            Total finger count.
-
-        Returns
-        -------
-        int
-            Updated Total finger count.
-        """
-
         # increase the number according to parsed image areas for fingers
         for coordinate in self.finger_coor:
             if landmarks[coordinate[0]][1] < landmarks[coordinate[1]][1]:
@@ -130,25 +80,8 @@ class Finger_counter():
             finger_count += 1
 
         return finger_count
-    
+
     def _extract_landmarks(self, image:np.ndarray, results:type, referanced_hand_index:int = 0)->list:
-        """Extracts the landmarks from result object.
-
-        Parameters
-        ----------
-        image : np.ndarray
-            Target image
-        results : type
-            It contains landmarks properties.   _
-        referanced_hand_index : int, optional
-            THe index number of the selected hand to process, by default 0
-
-        Returns
-        -------
-        list
-            listo the landmarks coordinates.
-        """
-
         # finding out the land mark coordiantes
         landmarks = list()
         for landmark in results.multi_hand_landmarks[referanced_hand_index].landmark:
